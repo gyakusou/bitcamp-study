17a4a00c6aab66481c7194be2fabf0a1140c7601
gyaksuou <gyaksuou@naver.com>
2019-12-10 18:20:21 +0900

    1210 project

---
+++ myproject/src/main/java/com/bitcamp/myproject/App2.java
@@ -0,0 +1,15 @@
+package com.bitcamp.myproject;
+
+public class App2 {
+  public static void main(String[] args) {
+    
+    System.out.println("번호: 1");
+    System.out.println("이름: 홍성준");
+    System.out.println("이메일: gyakusou@naver.com");
+    System.out.println("암호: 1111");
+    System.out.println("사진: 1.jpg");
+    System.out.println("전화: 010-5245-2421");
+    System.out.println("가입일: 2019-1210");
+    
+  }
+}
---
+++ myproject/src/main/java/com/bitcamp/myproject/App3.java
@@ -0,0 +1,16 @@
+package com.bitcamp.myproject;
+
+public class App3 {
+  public static void main(String[] args) {
+
+    System.out.println("번호: 1");
+    System.out.println("내용: 로그인 오류입니다.");
+    System.out.println("내용: 게시글입니다.");
+    System.out.println("내용: 게시글을 작성해 주세요.");
+    System.out.println("내용: 게시글을 작성했습니다.");
+    System.out.println("내용: 게시글이 수정되었습니다.");
+    System.out.println("작성일: 2019-01-01");
+    System.out.println("조회수: 0");
+
+  }
+}

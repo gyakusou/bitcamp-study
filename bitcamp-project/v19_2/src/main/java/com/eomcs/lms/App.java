988455cf668dc18964d3d7bd315a84c73a4c974d
hongseongjun90 <hongseongjun90@gmail.com>
2020-01-09 18:45:04 +0900

    0109

--- bitcamp-project/v19_2/src/main/java/com/eomcs/lms/App.java
+++ bitcamp-project/v19_2/src/main/java/com/eomcs/lms/App.java
@@ -4,26 +4,23 @@ import java.util.Scanner;
 import com.eomcs.lms.handler.BoardHandler;
 import com.eomcs.lms.handler.LessonHandler;
 import com.eomcs.lms.handler.MemberHandler;
-import com.eomcs.util.Prompt;
 
 public class App {
-  
+
   static Scanner keyboard = new Scanner(System.in);
-  
+
   public static void main(String[] args) {
-    
-    Prompt prompt = new Prompt(keyboard);
-    
-    BoardHandler boardHandler = new BoardHandler(prompt);
-    LessonHandler lessonHandler = new LessonHandler(prompt);
-    MemberHandler memberHandler = new MemberHandler(prompt);
-    
+
+    BoardHandler boardHandler = new BoardHandler(keyboard);
+    LessonHandler lessonHandler = new LessonHandler(keyboard);
+    MemberHandler memberHandler = new MemberHandler(keyboard);
+
     String command;
-    
+
     do {
       System.out.print("\n명령> ");
       command = keyboard.nextLine();
-      
+
       switch (command) {
         case "/lesson/add":
           lessonHandler.addLesson();
@@ -75,11 +72,11 @@ public class App {
             System.out.println("실행할 수 없는 명령입니다.");
           }
       }
-      
+
     } while (!command.equalsIgnoreCase("quit"));
-    
+
     System.out.println("안녕!");
-    
+
     keyboard.close();
   }
 }
--- bitcamp-project/v19_2/src/main/java/com/eomcs/lms/handler/BoardHandler.java
+++ bitcamp-project/v19_2/src/main/java/com/eomcs/lms/handler/BoardHandler.java
@@ -4,29 +4,33 @@
 package com.eomcs.lms.handler;
 
 import java.sql.Date;
+import java.util.Scanner;
 import com.eomcs.lms.domain.Board;
 import com.eomcs.util.ArrayList;
-import com.eomcs.util.LinkedList;
-import com.eomcs.util.Prompt;
 
 public class BoardHandler {
-  
-  LinkedList<Board> boardList;
-  
-  Prompt prompt;
-  
-  public BoardHandler(Prompt prompt) {
-    this.prompt = prompt;
-    this.boardList = new LinkedList<>();
+
+  ArrayList<Board> boardList;
+
+  Scanner input;
+
+  public BoardHandler(Scanner input) {
+    this.input = input;
+    this.boardList = new ArrayList<>();
   }
-  
+
+  public BoardHandler(Scanner input, int capacity) {
+    this.input = input;
+    this.boardList = new ArrayList<>(capacity);
+  }
+
   public void listBoard() {
     // BoardList의 보관된 값을 받을 배열을 준비한다. 
     Board[] arr = new Board[this.boardList.size()];
 
     // toArray()에게 빈 배열을 넘겨서 복사 받는다.
     this.boardList.toArray(arr);
-    
+
     for (Board b : arr) {
       System.out.printf("%d, %s, %s, %d\n", 
           b.getNo(), b.getTitle(), b.getDate(), b.getViewCount());
@@ -35,73 +39,94 @@ public class BoardHandler {
 
   public void addBoard() {
     Board board = new Board();
-    
-    board.setNo(prompt.inputInt("번호? "));
-    board.setTitle(prompt.inputString("내용? "));
+
+    System.out.print("번호? ");
+    board.setNo(input.nextInt());
+    input.nextLine(); // 줄바꿈 기호 제거용
+
+    System.out.print("내용? ");
+    board.setTitle(input.nextLine());
+
     board.setDate(new Date(System.currentTimeMillis()));
     board.setViewCount(0);
-    
+
     this.boardList.add(board);
-    
+
     System.out.println("저장하였습니다.");
   }
-  
+
   public void detailBoard() {
-    int index = indexOfBoard(prompt.inputInt("번호? "));
-    
+    System.out.print("번호? ");
+    int no = input.nextInt();
+    input.nextLine(); // 숫자 뒤의 남은 공백 제거
+
+    // 게시글 번호로 객체를 찾는다.
+    int index = indexOfBoard(no);
+
     if (index == -1) {
       System.out.println("해당 번호의 게시글이 없습니다.");
       return;
     }
-    
+
     Board board = this.boardList.get(index);
     System.out.printf("번호: %d\n", board.getNo());
     System.out.printf("제목: %s\n", board.getTitle());
     System.out.printf("등록일: %s\n", board.getDate());
     System.out.printf("조회수: %d\n", board.getViewCount());
   }
-  
+
   public void updateBoard() {
-    int index = indexOfBoard(prompt.inputInt("번호? "));
-    
+    System.out.print("번호? ");
+    int no = input.nextInt();
+    input.nextLine(); // 숫자 뒤의 남은 공백 제거
+
+    // 게시글 번호로 객체를 찾는다.
+    int index = indexOfBoard(no);
+
     if (index == -1) {
       System.out.println("해당 번호의 게시글이 없습니다.");
       return;
     }
-    
+
     Board oldBoard = this.boardList.get(index);
-    Board newBoard = new Board();
-    
-    newBoard.setNo(oldBoard.getNo());
-    newBoard.setViewCount(oldBoard.getViewCount());
-    newBoard.setDate(new Date(System.currentTimeMillis()));
-    newBoard.setTitle(prompt.inputString(
-        String.format("내용(%s)? ", oldBoard.getTitle()), 
-        oldBoard.getTitle()));
-    
-    
-    if (newBoard.equals(oldBoard)) {
+
+    System.out.printf("내용(%s)? ", oldBoard.getTitle());
+    String title = input.nextLine();
+
+    if (title.length() == 0) {
       System.out.println("게시글 변경을 취소했습니다.");
       return;
     }
-    
+
+    Board newBoard = new Board();
+    newBoard.setNo(oldBoard.getNo());
+    newBoard.setViewCount(oldBoard.getViewCount());
+    newBoard.setTitle(title);
+    newBoard.setDate(new Date(System.currentTimeMillis()));
+
     this.boardList.set(index, newBoard);
+
     System.out.println("게시글을 변경했습니다.");
   }
-  
+
   public void deleteBoard() {
-    int index = indexOfBoard(prompt.inputInt("번호? "));
-    
+    System.out.print("번호? ");
+    int no = input.nextInt();
+    input.nextLine(); // 숫자 뒤의 남은 공백 제거
+
+    // 게시글 번호로 객체를 찾는다.
+    int index = indexOfBoard(no);
+
     if (index == -1) {
       System.out.println("해당 번호의 게시글이 없습니다.");
       return;
     }
-    
+
     this.boardList.remove(index);
-    
+
     System.out.println("게시글을 삭제했습니다.");
   }
-  
+
   private int indexOfBoard(int no) {
     for (int i = 0; i < this.boardList.size(); i++) {
       if (this.boardList.get(i).getNo() == no) {

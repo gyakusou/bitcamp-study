013343bf37d29c102256d62645b6b1c28a5f9c21
hongseongjun90 <hongseongjun90@gmail.com>
2020-01-31 18:20:28 +0900

    v31_2 sf

--- bitcamp-project/README.md
+++ bitcamp-project/README.md
@@ -1,93 +1,54 @@
-# v31 - `Observer` 디자인 패턴의 활용
+# v31_2 - 애플리케이션을 시작하거나 종료할 때 작업 할 옵저버를 추가한다.
 
 ## 학습목표
 
-- 특정 상태에서 수행되는 코드를 캡슐화하여 분리할 수 있다.
-- `Observer` 디자인 패턴의 용도와 이점을 이해한다.
+- 옵저버 디자인 패턴에 따라 옵저버를 정의할 수 있다.
 
 ## 실습 소스 및 결과
 
-- src/main/java/com/eomcs/lms/App.java 변경
-- src/main/java/com/eomcs/context/ApplicationContextListener.java 추가
 - src/main/java/com/eomcs/lms/DataLoaderListener.java 추가
+- src/main/java/com/eomcs/lms/App.java 변경
 
 ## 실습  
 
-### 훈련 1: App 클래스에 스태틱 필드와 메서드를 인스턴스 멤버로 전환한다.
+### 훈련 1: 애플리케이션을 시작하거나 종료할 때 데이터를 로딩하고 저장 할 옵저버를 만든다.
 
-- App.java (App.java.01)
-    - 스태틱 필드와 스태틱 메서드를 인스턴스 필드와 인스턴스 메서드로 전환한다.
-    - 보통 실무에서는 클래스의 일반적인 구조로 인스턴스 필드와 메서드를 사용한다.
-
-### 훈련 2: 애플리케이션 시작하거나 종료할 때 호출될 옵저버의 규칙을 정의한다.
-
-- ApplicationContextListener.java (ApplicationContextListener.java.01) (Observer)
-    - Observer가 갖춰야 할 규칙을 정의한다.
-    - 애플리케이션이 시작할 때 자동으로 호출할 메서드의 규칙을 정의한다.
-    - 애플리케이션을 종료하기 전에 자동으로 호출할 메서드의 규칙을 정의한다.
+- DataLoaderListener.java (DataLoaderListener.java.01)추가
+    - ApplicationContextListener를 구현한다.
+    - 테스트 할 용도로 간단하게 구현한다.
     
-### 훈련 3: App 객체에 옵저버를 등록하고 제거하는 기능을 추가한다.
+### 훈련 2: DataLoaderListener 옵저버를 App 객체에 등록하고 실행 확인한다.
 
-- App.java (App.java.02)
-    - 옵저버를 등록하는 메서드를 추가한다.
-    - 옵저버를 제거하는 메서드를 추가한다.
-    - 애플리케이션을 시작할 때 옵저버를 호출한다.
-    - 애플리케이션을 종료할 때 옵저버를 호출한다.
+- App.java 변경
+    - DataLoaderListener 객체를 생성한 후 App 객체에 등록한다.
+    - 실행하여 옵저버가 동작하는 지를 확인한다.
+  
+### 훈련 3: DataLoaderListener 옵저버에서 데이터를 로딩하고 저장한다.
+
+- DataLoaderListener.java 변경 (App.java.02)
+    - App 클래스에 있는 List 객체를 이 클래스로 옮긴다.
+    - App 클래스에 있는 데이터 로딩, 저장 관련 메서드를 이 클래스로 옮긴다.
+- App.java 변경    
+    - List 객체를 필드에서 제거한다. (DataloaderListener 가 할 일이다.)
+    - 데이터 로딩,  저장 관련 메서드를 제거한다. (DataloaderListener 가 할 일이다.)
+    - 데이터 로딩 호출 코드를 제거한다. (DataloaderListener 가 할 일이다.)
+    - 데이터 저장 호출 코드를 제거한다. (DataloaderListener 가 할 일이다.)
     
-### 훈련 4: 애플리케이션을 시작하거나 종료할 때 데이터를 로딩하고 저장하는 옵저버를 만들고 등록한다.
+### 훈련 4: App 클래스가 옵저버의 결과물을 사용 할 수 있게 하라!
 
-- DataLoaderListener.java (DataLoaderListener.java.01)
-    - `ApplicationContextListener`를 구현한다. 즉 `Observer`를 만든다.
-    - `App` 클래스에 있는 파일 입출력 코드를 이 클래스로 옮긴다.
-    - 애플리케이션이 시작될 때 파일로부터 데이터를 읽는다.
-    - 애플리케이션이 종료될 때 파일로 데이터를 쓴다.
-- App.java (App.java.01)
-    - `Observer` 디자인 패턴을 적용하기 전에 코드를 먼저 정리한다.
-    - 기존의 스태틱 멤버를 인스턴스 멤버로 전환한다.
-    - main()에 작성된 코드를 인스턴스 메서드로 분리하여 정리한다.
-    - `Observer(예: ApplicationContextListener)`를 등록할 `addApplicationContextListener()`를 추가한다.
+- ApplicationContextListener.java (변경)
+  - contextInitialized() 에 Map 파라미터를 추가한다.
+  - contextDestroyed() 에 Map 파라미터를 추가한다.
+    
+### 훈련 5: DataLoaderListener의 작업 결과를 Map 객체를 통해 공유하라
 
-#### 실행 결과
+- DataLoaderListener.java (변경)
+  - 데이터 로딩 결과를 Map 객체에 보관한다.
+  
+### 훈련 6: DataLoaderListener 에서 준비한 List 객체를 Command 에게 전달하라
 
-`App`의 실행해도 이전에 저장한 데이터가 출력되지 않는다.
-```
-데이터를 읽어옵니다.
-명령> /board/list
-
-명령> /board/add
-번호? 100
-내용? test..
-저장하였습니다.
-
-명령> /board/list
-100, test..              , 2018-11-09, 0
-
-명령> quit
-안녕!
-데이터를 저장합니다.
-```
-
-종료한 후에 다시 실행해도 이전에 추가한 데이터가 출력되지 않는다.
-```
-데이터를 읽어옵니다.
-명령> /board/list
-
-```
-
-이유는 DataLoaderListener에서 준비한 ArrayList와 LinkedList를 핸들러에서 사용하지 않기 때문이다. 다음 단계에서 처리한다.
-
-### 과제 2: DataLoaderListener에서 준비한 객체를 다른 객체와 공유하라.
-
-- ApplicationContextListener.java
-    - `App` 객체와의 값을 공유하기 위해 `contextInitialized()` 와 `contextDestroyed()`에 Map 파라미터를 추가한다.
-- DataLoaderListener.java
-    - `ApplicationContextListener`의 변경에 따라 코드를 바꾼다.
-    - 파일에서 수업, 회원, 게시글 데이터를 읽어 들인 후 그 데이터가 보관된 컬렉션 객체를 Map에 공유한다. 
-- App.java
-    - `Map` 객체를 통해 `Observer`와 `App` 사이에서 값을 공유한다.
-    - `DataLoaderListener`가 준비한 컬렉션 객체를 핸들러에 넘긴다.
-
-#### 실행 결과
-
-이제 `App`의 실행 결과는 이전 버전과 같다. 애플리케이션을 실행하면 정상적으로 파일에서 데이터를 읽어오고, 종료하면 파일로 데이터를 출력할 것이다.
+- App.java (변경)
+  - 애플리케이션이 시작될 때 옵저버를 실행한 후 Map에 저장된 작업결과를 꺼내
+    command 객체에 전달한다.
+    
 
--- bitcamp-project/src/main/java/com/eomcs/lms/App.java
+++ bitcamp-project/src/main/java/com/eomcs/lms/App.java
@@ -1,21 +1,13 @@
 package com.eomcs.lms;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.util.ArrayDeque;
-import java.util.ArrayList;
 import java.util.Deque;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Queue;
 import java.util.Scanner;
 import java.util.Set;
@@ -43,52 +35,65 @@ import com.eomcs.lms.handler.MemberListCommand;
 import com.eomcs.lms.handler.MemberUpdateCommand;
 import com.eomcs.util.Prompt;
 
-public class App { // 31
+public class App {
 
   Scanner keyboard = new Scanner(System.in);
 
   Deque<String> commandStack = new ArrayDeque<>();
   Queue<String> commandQueue = new LinkedList<>();
 
-  List<Lesson> lessonList = new ArrayList<>();
-  List<Member> memberList = new ArrayList<>();
-  List<Board> boardList = new ArrayList<>();
-
   // 옵저버 목록을 관리할 객체 준비
   // - 같은 인스턴스를 중복해서 등록하지 않도록 한다.
-  // - set은 등록 순서를 따지지 않는다.
-  Set<ApplicationContextListener> listeners = new HashSet<>(); // 31 2
+  // - Set은 등록 순서를 따지지 않는다.
+  Set<ApplicationContextListener> listeners = new HashSet<>();
+
+  // 옵저버와 공유할 값을 보관할 객체를 준비한다.
+  Map<String, Object> context = new HashMap<>();
 
   // 옵저버를 등록하는 메서드이다.
-  public void addApplicationcontextListener(ApplicationContextListener listener) { // 31 2
+  public void addApplicationContextListener(ApplicationContextListener listener) {
     listeners.add(listener);
   }
 
-  public void removeaddApplicationcontextListener(ApplicationContextListener listener) { // 31 2
+  // 옵저버를 제거하는 메서드이다.
+  public void removeApplicationContextListener(ApplicationContextListener listener) {
     listeners.remove(listener);
   }
 
-  // 애플리케이션이 시작될 때 등록된 리스너에게 알린다.
-  private void notifyApplicationInitialized() { // 31 2
+  // 애플리케이션이 시작되면, 등록된 리스너에게 알린다.
+  private void notifyApplicationInitialized() {
     for (ApplicationContextListener listener : listeners) {
-      listener.contextInitatliazed();
+      // 옵저버를 실행할 때 작업 결과를 리턴 받을 수 있도록 바구니를 넘긴다.
+      // 물론 옵저버에게 전달할 값이 있으면 넘기기 전에 바구니에 담도록 한다.
+      // 파라미터 Map과 같은 객체를 사용하면 이런 점에서 편하다.
+      // 즉 파라미터로 값을 전달(IN)하고 리턴(OUT) 받을 수 있다.
+      listener.contextInitialized(context);
     }
   }
 
-  // 애플리케이션이 종료되면 등록된 리스너에게 알린다.
-  private void notifyApplicationDestroyed() { // 31 2
+  // 애플리케이션이 종료되면, 등록된 리스너에게 알린다.
+  private void notifyApplicationDestroyed() {
     for (ApplicationContextListener listener : listeners) {
-      listener.contextDestroyed();
+      // 옵저버를 실행할 때 작업 결과를 리턴 받을 수 있도록 바구니를 넘긴다.
+      // 물론 옵저버에게 전달할 값이 있으면 넘기기 전에 바구니에 담도록 한다.
+      // 파라미터 Map과 같은 객체를 사용하면 이런 점에서 편하다.
+      // 즉 파라미터로 값을 전달(IN)하고 리턴(OUT) 받을 수 있다.
+      listener.contextDestroyed(context);
     }
   }
 
-  public void service() { // 31
+  @SuppressWarnings("unchecked")
+  public void service() {
 
-    notifyApplicationInitialized(); // 31 2
+    // 애플리케이션이 시작되면 등록된 옵저버를 실행한다.
+    // 즉 DataLoaderListener를 실행한다.
+    notifyApplicationInitialized();
 
-    loadLessonData();
-    loadMemberData();
-    loadBoardData();
+    // 옵저버의 실행이 끝났으면 DataLoaderListener 옵저버가 준비한
+    // List 객체를 꺼내보자!
+    List<Board> boardList = (List<Board>) context.get("boardList");
+    List<Lesson> lessonList = (List<Lesson>) context.get("lessonList");
+    List<Member> memberList = (List<Member>) context.get("memberList");
 
     Prompt prompt = new Prompt(keyboard);
     HashMap<String, Command> commandMap = new HashMap<>();
@@ -155,15 +160,11 @@ public class App { // 31
 
     keyboard.close();
 
-    saveLessonData();
-    saveMemberData();
-    saveBoardData();
-
-    notifyApplicationDestroyed(); // 31 2 //////////////////////////////////////////////////////////
+    notifyApplicationDestroyed();
 
   } // service()
 
-  private void printCommandHistory(Iterator<String> iterator) { // 31
+  private void printCommandHistory(Iterator<String> iterator) {
     int count = 0;
     while (iterator.hasNext()) {
       System.out.println(iterator.next());
@@ -179,104 +180,13 @@ public class App { // 31
     }
   }
 
-  @SuppressWarnings("unchecked")
-  private void loadLessonData() { // 31
-    File file = new File("./lesson.ser2");
-
-    try (ObjectInputStream in =
-        new ObjectInputStream(new BufferedInputStream(new FileInputStream(file)))) {
-
-      lessonList = (List<Lesson>) in.readObject();
-
-      System.out.printf("총 %d 개의 수업 데이터를 로딩했습니다.\n", lessonList.size());
-
-    } catch (Exception e) {
-      System.out.println("파일 읽기 중 오류 발생! - " + e.getMessage());
-    }
-  }
-
-  private void saveLessonData() { // 31
-    File file = new File("./lesson.ser2");
-
-    try (ObjectOutputStream out =
-        new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream(file)))) {
-      out.writeObject(lessonList);
-
-      System.out.printf("총 %d 개의 수업 데이터를 저장했습니다.\n", lessonList.size());
-
-    } catch (IOException e) {
-      System.out.println("파일 쓰기 중 오류 발생! - " + e.getMessage());
-    }
-  }
-
-  @SuppressWarnings("unchecked")
-  private void loadMemberData() { // 31
-    File file = new File("./member.ser2");
-
-    try (ObjectInputStream in =
-        new ObjectInputStream(new BufferedInputStream(new FileInputStream(file)))) {
-
-      memberList = (List<Member>) in.readObject();
-
-      System.out.printf("총 %d 개의 회원 데이터를 로딩했습니다.\n", memberList.size());
-
-    } catch (Exception e) {
-      System.out.println("파일 읽기 중 오류 발생! - " + e.getMessage());
-    }
-  }
-
-  private void saveMemberData() { // 31
-    File file = new File("./member.ser2");
-
-    try (ObjectOutputStream out =
-        new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream(file)))) {
-
-      out.writeObject(memberList);
-
-
-      System.out.printf("총 %d 개의 회원 데이터를 저장했습니다.\n", memberList.size());
-
-    } catch (IOException e) {
-      System.out.println("파일 쓰기 중 오류 발생! - " + e.getMessage());
-    }
-  }
-
-  @SuppressWarnings("unchecked")
-  private void loadBoardData() { // 31
-    File file = new File("./board.ser2");
-
-    try (ObjectInputStream in =
-        new ObjectInputStream(new BufferedInputStream(new FileInputStream(file)))) {
-
-
-      boardList = (List<Board>) in.readObject();
-
-      System.out.printf("총 %d 개의 게시물 데이터를 로딩했습니다.\n", boardList.size());
-
-    } catch (Exception e) {
-      System.out.println("파일 읽기 중 오류 발생! - " + e.getMessage());
-    }
-  }
-
-  private void saveBoardData() { // 31
-    File file = new File("./board.ser2");
-
-    try (ObjectOutputStream out =
-        new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream(file)))) {
-
-      out.writeObject(boardList);
-
-      System.out.printf("총 %d 개의 게시물 데이터를 저장했습니다.\n", boardList.size());
-
-    } catch (IOException e) {
-      System.out.println("파일 쓰기 중 오류 발생! - " + e.getMessage());
-
-    }
-  }
-
-  public static void main(String[] args) { // 31
+  public static void main(String[] args) {
     App app = new App();
-    app.service(); // app에 대해서 service 호출
+
+    // 애플리케이션의 상태를 정보를 받을 옵저버를 등록한다.
+    app.addApplicationContextListener(new DataLoaderListener());
+
+    app.service();
   }
 }
 

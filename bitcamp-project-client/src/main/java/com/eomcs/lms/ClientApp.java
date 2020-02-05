d360c0cdf31023c0a2c183e21176ba304d3ae2e2
hongseongjun90 <hongseongjun90@gmail.com>
2020-02-05 19:46:27 +0900

    연습

--- bitcamp-project-client/src/main/java/com/eomcs/lms/ClientApp.java
+++ bitcamp-project-client/src/main/java/com/eomcs/lms/ClientApp.java
@@ -1,162 +1,58 @@
-// LMS 클라이언트
 package com.eomcs.lms;
 
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.PrintStream;
 import java.net.Socket;
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Scanner;
-import com.eomcs.lms.handler.BoardAddCommand;
-import com.eomcs.lms.handler.BoardDeleteCommand;
-import com.eomcs.lms.handler.BoardDetailCommand;
-import com.eomcs.lms.handler.BoardListCommand;
-import com.eomcs.lms.handler.BoardUpdateCommand;
-import com.eomcs.lms.handler.Command;
-import com.eomcs.lms.handler.LessonAddCommand;
-import com.eomcs.lms.handler.LessonDeleteCommand;
-import com.eomcs.lms.handler.LessonDetailCommand;
-import com.eomcs.lms.handler.LessonListCommand;
-import com.eomcs.lms.handler.LessonUpdateCommand;
-import com.eomcs.lms.handler.MemberAddCommand;
-import com.eomcs.lms.handler.MemberDeleteCommand;
-import com.eomcs.lms.handler.MemberDetailCommand;
-import com.eomcs.lms.handler.MemberListCommand;
-import com.eomcs.lms.handler.MemberUpdateCommand;
-import com.eomcs.util.Prompt;
 
 public class ClientApp {
 
-  Scanner keyboard = new Scanner(System.in);
-  Prompt prompt = new Prompt(keyboard);
-
-  public void service() {
+  public static void main(String[] args) throws Exception {
 
     String serverAddr = null;
     int port = 0;
 
+    Scanner keyScan = new Scanner(System.in);
+
     try {
-      serverAddr = prompt.inputString("서버? ");
-      port = prompt.inputInt("포트? ");
+
+      System.out.print("서버? ");
+      serverAddr = keyScan.nextLine();
+
+      System.out.print("포트? ");
+      port = Integer.parseInt(keyScan.nextLine());
 
     } catch (Exception e) {
-      System.out.println("서버 주소 또는 포트 번호가 유효하지 않습니다!");
-      keyboard.close();
+      System.out.println("서버주소 or 포트번호가 유효하지 않습니다.");
+      keyScan.close();
       return;
     }
 
-    try (Socket socket = new Socket(serverAddr, port);
-        ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
-        ObjectInputStream in = new ObjectInputStream(socket.getInputStream())) {
+    try (
 
-      System.out.println("서버와 연결을 되었음!");
+        Socket socket = new Socket(serverAddr, port);
+        PrintStream out = new PrintStream(socket.getOutputStream());
+        Scanner in = new Scanner(socket.getInputStream())) {
 
-      processCommand(out, in);
+      System.out.println("서버와 연결됨");
+      System.out.print("서버에 보낼 메세지: ");
+      String sendMsg = keyScan.nextLine();
 
-      System.out.println("서버와 연결을 끊었음!");
+      out.println(sendMsg);
+      System.out.println("서버에 메세지를 전송");
+
+      String message = in.nextLine();
+      System.out.println("서버로부터 메세지 수신");
+
+      System.out.println("서버: " + message);
+
+      System.out.println("서버와 연결 끊음");
 
     } catch (Exception e) {
-      System.out.println("예외 발생:");
+      System.out.println("예외발생");
       e.printStackTrace();
     }
-
-    keyboard.close();
-  }
-
-  private void processCommand(ObjectOutputStream out, ObjectInputStream in) {
-
-    Deque<String> commandStack = new ArrayDeque<>();
-    Queue<String> commandQueue = new LinkedList<>();
-
-    HashMap<String, Command> commandMap = new HashMap<>();
-    commandMap.put("/board/list", new BoardListCommand(out, in));
-    commandMap.put("/board/add", new BoardAddCommand(out, in, prompt));
-    commandMap.put("/board/detail", new BoardDetailCommand(out, in, prompt));
-    commandMap.put("/board/update", new BoardUpdateCommand(out, in, prompt));
-    commandMap.put("/board/delete", new BoardDeleteCommand(out, in, prompt));
-
-    commandMap.put("/member/list", new MemberListCommand(out, in));
-    commandMap.put("/member/add", new MemberAddCommand(out, in, prompt));
-    commandMap.put("/member/detail", new MemberDetailCommand(out, in, prompt));
-    commandMap.put("/member/update", new MemberUpdateCommand(out, in, prompt));
-    commandMap.put("/member/delete", new MemberDeleteCommand(out, in, prompt));
-
-    commandMap.put("/lesson/list", new LessonListCommand(out, in));
-    commandMap.put("/lesson/add", new LessonAddCommand(out, in, prompt));
-    commandMap.put("/lesson/detail", new LessonDetailCommand(out, in, prompt));
-    commandMap.put("/lesson/update", new LessonUpdateCommand(out, in, prompt));
-    commandMap.put("/lesson/delete", new LessonDeleteCommand(out, in, prompt));
-
-    try {
-      while (true) {
-        String command;
-        command = prompt.inputString("\n명령> ");
-
-        if (command.length() == 0)
-          continue;
-
-        if (command.equals("quit") || command.equals("/server/stop")) {
-          out.writeUTF(command);
-          out.flush();
-          System.out.println("서버: " + in.readUTF());
-          System.out.println("안녕!");
-          break;
-        } else if (command.equals("history")) {
-          printCommandHistory(commandStack.iterator());
-          continue;
-        } else if (command.equals("history2")) {
-          printCommandHistory(commandQueue.iterator());
-          continue;
-        }
-
-        commandStack.push(command);
-
-        commandQueue.offer(command);
-
-        Command commandHandler = commandMap.get(command);
-
-        if (commandHandler != null) {
-          try {
-            commandHandler.execute();
-          } catch (Exception e) {
-            e.printStackTrace();
-            System.out.printf("명령어 실행 중 오류 발생: %s\n", e.getMessage());
-          }
-        } else {
-          System.out.println("실행할 수 없는 명령입니다.");
-        }
-      }
-    } catch (Exception e) {
-      System.out.println("프로그램 실행 중 오류 발생!");
-    }
-
-    keyboard.close();
-
-  }
-
-  private void printCommandHistory(Iterator<String> iterator) {
-    int count = 0;
-    while (iterator.hasNext()) {
-      System.out.println(iterator.next());
-      count++;
-
-      if ((count % 5) == 0) {
-        String str = prompt.inputString(":");
-        if (str.equalsIgnoreCase("q")) {
-          break;
-        }
-      }
-    }
-  }
-
-  public static void main(String[] args) throws Exception {
-    System.out.println("클라이언트 수업 관리 시스템입니다.");
-
-    ClientApp app = new ClientApp();
-    app.service();
+    keyScan.close();
   }
 }
+
+// v32_2 까지했음

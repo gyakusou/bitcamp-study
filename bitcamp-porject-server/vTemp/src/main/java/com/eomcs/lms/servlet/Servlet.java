c5a22ad8cbed8d718a7d5834c30aa8d309fea527
hongseongjun90 <hongseongjun90@gmail.com>
2020-02-06 11:02:11 +0900

    v32_7

--- bitcamp-porject-server/vTemp/src/main/java/com/eomcs/lms/servlet/Servlet.java
+++
@@ -1,8 +0,0 @@
-package com.eomcs.lms.servlet;
-
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-
-public interface Servlet {
-  void service(ObjectInputStream in, ObjectOutputStream out) throws Exception; // 이 메서드는 예외 발생 가능
-}
--- bitcamp-porject-server/vTemp/src/test/java/com/eomcs/lms/ServerAppTest.java
+++
@@ -1,8 +0,0 @@
-package com.eomcs.lms;
-
-// public class ServerAppTest {
-// @Test
-// public void testBoardList() {
-
-// }
-// }

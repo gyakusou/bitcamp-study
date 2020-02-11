9645a341b6a4022a1aa481d4a5104b8abad794d0
hongseongjun90 <hongseongjun90@gmail.com>
2020-02-11 13:16:14 +0900

    0211

--- bitcamp-java/src/main/java/com/eomcs/net/ex06/HttpClient.java
+++ bitcamp-java/src/main/java/com/eomcs/net/ex06/HttpClient.java
@@ -1,9 +1,10 @@
 // HTTP 클라이언트 만들기
-package com.eomcs.net.ex07;
+package com.eomcs.net.ex06;
 
 import java.io.PrintStream;
 import java.net.Socket;
 import java.util.Scanner;
+// CRLF = carriage return line feed 줄바꿈해야 한다.
 
 // HTTP 요청 프로토콜
 // ---------------------------------
@@ -24,9 +25,12 @@ public class HttpClient {
     Scanner in = new Scanner(socket.getInputStream());
 
     // HTTP 요청 프로토콜에 따라 서버에 데이터 전송
-    out.println("GET /news/108939 HTTP/1.1");
-    out.println("Host: www.itworld.co.kr");
-    out.println();
+    // => macOS에서 JVM을 실행할 때 
+    // println()은 문자열 뒤에 0a(LF) 코드만 붙인다.
+    // => 따라서 macOS에서 실행할 때는 다음과 같이 명확하게 CRLF 코드를 붙인다.
+    out.println("GET /news/108939 HTTP/1.1\r\n");
+    out.println("Host: itworld.co.kr\r\n");
+    out.println("\r\n");
 
     // HTTP 응답 프로토콜에 따라 서버가 보낸 데이터를 수신
     while (true) {
@@ -42,14 +46,5 @@ public class HttpClient {
     socket.close();
   }
 }
-
-
-
-
-
-
-
-
-
 
 
--- bitcamp-java/src/main/java/com/eomcs/net/ex06/HttpServer.java
+++ bitcamp-java/src/main/java/com/eomcs/net/ex06/HttpServer.java
@@ -1,21 +1,25 @@
 // HTTP 서버 만들기
-package com.eomcs.net.ex07;
+package com.eomcs.net.ex06;
 
 import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
+// CRLF = carriage return line feed 줄바꿈해야 한다.
+
 // HTTP 응답 프로토콜
-//--------------------------------
+// --------------------------------
 // HTTP/1.1 200 OK(CRLF)
 // Content-Type: text/html; charset=UTF-8(CRLF)
 // (CRLF)
-// 보낼 데이터 
-//--------------------------------
+// 보낼 데이터
+// --------------------------------
 public class HttpServer {
   public static void main(String[] args) throws Exception {
     ServerSocket ss = new ServerSocket(8888);
+
+    System.out.println("서버 실행");
 
     while (true) {
       Socket socket = ss.accept();
@@ -27,14 +31,15 @@ public class HttpServer {
         String str = in.nextLine();
         System.out.println(str);
         if (str.equals(""))
+          // 빈줄을 만나게 되면 멈춘다.
           break;
       }
 
       // HTTP 응답 프로토콜에 따라 클라이언트에게 데이터를 보낸다.
-      out.println("HTTP/1.1 200 OK");
-      out.println("Content-Type: text/html; charset=UTF-8");
-      out.println();
-      out.println("<html><body><h1>안녕!-엄진영</h1></body></html>");
+      out.println("HTTP/1.1 200 OK\r\n");
+      out.println("Content-Type: text/html; charset=UTF-8\r\n");
+      out.println("\r\n");
+      out.println("<html><body><h1>안녕!-홍성준</h1></body></html>\r\n");
 
       out.close();
       in.close();
@@ -45,8 +50,6 @@ public class HttpServer {
 }
 
 
-
-
-
-
+// 1. 서버실행
+// 2. 웹 브라우저 http://localhost:8888/
 

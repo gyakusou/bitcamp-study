3cd1ecf0326596bd941658b056a92c9baed103b6
hongseongjun90 <hongseongjun90@gmail.com>
2020-02-14 12:10:09 +0900

    0214

--- bitcamp-java/src/main/java/com/eomcs/concurrent/ex6/Exam0210.java
+++ bitcamp-java/src/main/java/com/eomcs/concurrent/ex6/Exam0210.java
@@ -17,7 +17,6 @@ public class Exam0210 {
 
       public void setCount(int count) {
         this.count = count;
-
         synchronized (this) {
           notify();
         }
@@ -25,18 +24,14 @@ public class Exam0210 {
 
       @Override
       public void run() {
-
         synchronized (this) {
           try {
             while (true) {
               wait();
-              System.out.println("카운트 시작");
               for (int i = count; i > 0; i--) {
-                System.out.printf("[%s] %d\n ", getName(), i);
+                System.out.printf("[%s] %d\n", getName(), i);
                 Thread.sleep(1000);
               }
-              // 스레드의 작업이 끝났으면 스레드풀로 돌아가야 한다.
-
             }
           } catch (Exception e) {
             e.printStackTrace();
@@ -53,16 +48,16 @@ public class Exam0210 {
         t1.start();
         list.add(t1);
 
-        MyThread t2 = new MyThread("-------->");
+        MyThread t2 = new MyThread("------");
         t2.start();
         list.add(t2);
 
-        MyThread t3 = new MyThread("$$$$$");
+        MyThread t3 = new MyThread("$$$");
         t3.start();
         list.add(t3);
       }
 
-      // 스레드 풀에서 한개의 스레드를 꺼낸다.
+      // 스레드 풀에서 한 개의 스레드를 꺼낸다.
       public MyThread get() {
         if (list.size() > 0) {
           return list.remove(0);
@@ -70,13 +65,13 @@ public class Exam0210 {
         return null;
       }
 
-      // 스레드를 다 쓴 후에는 다시 쓰레드 풀에 돌려준다.
+      // 스레드를 다 쓴 후에는 다시 스레드 풀에 돌려준다.
       public void add(MyThread t) {
         list.add(t);
       }
     }
 
-    // 스레드 풀 준비
+    // 스레드풀 준비!
     MyThreadPool threadPool = new MyThreadPool();
 
     Scanner keyScan = new Scanner(System.in);
@@ -87,21 +82,22 @@ public class Exam0210 {
       if (str.equals("quit")) {
         break;
       }
+
       int count = Integer.parseInt(str);
 
       // 스레드풀에서 스레드를 한 개 꺼낸다.
       MyThread t = threadPool.get();
       if (t == null) {
-        System.out.println("남는 스레드가 없습니다.");
+        System.out.println("남는 스레드가 없습니다!");
         continue;
       }
 
-      // 스레드풀에서 스레드를 받았다면 카운트를 다시 시작시킨다.
+      // 스레드풀에서 스레드를 받았다면 카운트를 시작시킨다.
       t.setCount(count);
     }
-    System.out.println("main 스레드 종료");
+
+    System.out.println("main 스레드 종료!");
     keyScan.close();
   }
 }
-
 

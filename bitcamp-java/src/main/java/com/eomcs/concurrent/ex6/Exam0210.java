f6f50f75bdbbbd7b93c8b833ade5e08ad6e95c0f
hongseongjun90 <hongseongjun90@gmail.com>
2020-02-14 14:14:47 +0900

    0214

--- bitcamp-java/src/main/java/com/eomcs/concurrent/ex6/Exam0210.java
+++ bitcamp-java/src/main/java/com/eomcs/concurrent/ex6/Exam0210.java
@@ -1,4 +1,4 @@
-// 멀티 스레드 재사용
+// 멀티 스레드 재사용 - Pooling 기법을 이용하여 생성된 객체를 재활용하기 // 플라이웨이트 기법 = 풀링 기법
 package com.eomcs.concurrent.ex6;
 
 import java.util.ArrayList;
@@ -6,72 +6,92 @@ import java.util.Scanner;
 
 public class Exam0210 {
 
+  static class MyThread extends Thread {
+    ThreadPool pool;
+    int count;
+
+    public MyThread(String name, ThreadPool pool) {
+      super(name);
+      this.pool = pool;
+    }
+
+    public void setCount(int count) {
+      this.count = count;
+      synchronized (this) {
+        notify();
+      }
+    }
+
+    @Override
+    public void run() {
+      synchronized (this) {
+        try {
+          while (true) {
+            // 작업하라는 알림이 올 때 까지 기다린다.
+            wait();
+
+            // 알림이 오면 작업을 실행한다.
+            for (int i = count; i > 0; i--) {
+              System.out.printf("[%s] %d\n", getName(), i);
+              Thread.sleep(2000);
+            }
+
+            // 작업이 끝났으면 스레드풀로 돌아간다.
+            pool.add(this);
+          }
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
+      }
+    }
+  }
+
+  interface ThreadPool {
+
+    Thread get();
+
+    void add(Thread obj);
+  }
+
+  static class MyThreadPool implements ThreadPool {
+    ArrayList<MyThread> list = new ArrayList<>();
+
+    public MyThreadPool() { // this 어디 소속인지 알려준다.
+      MyThread t1 = new MyThread("1번 스레드==>", this);
+      t1.start();
+      list.add(t1);
+
+      MyThread t2 = new MyThread("2번 스레드***", this);
+      t2.start();
+      list.add(t2);
+
+      MyThread t3 = new MyThread("3번 스레드&&&", this);
+      t3.start();
+      list.add(t3);
+    }
+
+    // 스레드 풀에서 한 개의 스레드를 꺼낸다.
+    @Override
+    public MyThread get() {
+      if (list.size() > 0) {
+        return list.remove(0);
+      }
+      return null;
+    }
+
+    // 스레드를 다 쓴 후에는 다시 스레드 풀에 돌려준다.
+    @Override
+    public void add(Thread t) {
+      list.add((MyThread) t);
+
+    }
+  }
+
+  // 스레드풀 준비!
+
+
   public static void main(String[] args) {
 
-    class MyThread extends Thread {
-      int count;
-
-      public MyThread(String name) {
-        super(name);
-      }
-
-      public void setCount(int count) {
-        this.count = count;
-        synchronized (this) {
-          notify();
-        }
-      }
-
-      @Override
-      public void run() {
-        synchronized (this) {
-          try {
-            while (true) {
-              wait();
-              for (int i = count; i > 0; i--) {
-                System.out.printf("[%s] %d\n", getName(), i);
-                Thread.sleep(1000);
-              }
-            }
-          } catch (Exception e) {
-            e.printStackTrace();
-          }
-        }
-      }
-    }
-
-    class MyThreadPool {
-      ArrayList<MyThread> list = new ArrayList<>();
-
-      public MyThreadPool() {
-        MyThread t1 = new MyThread("=>");
-        t1.start();
-        list.add(t1);
-
-        MyThread t2 = new MyThread("------");
-        t2.start();
-        list.add(t2);
-
-        MyThread t3 = new MyThread("$$$");
-        t3.start();
-        list.add(t3);
-      }
-
-      // 스레드 풀에서 한 개의 스레드를 꺼낸다.
-      public MyThread get() {
-        if (list.size() > 0) {
-          return list.remove(0);
-        }
-        return null;
-      }
-
-      // 스레드를 다 쓴 후에는 다시 스레드 풀에 돌려준다.
-      public void add(MyThread t) {
-        list.add(t);
-      }
-    }
-
-    // 스레드풀 준비!
     MyThreadPool threadPool = new MyThreadPool();
 
     Scanner keyScan = new Scanner(System.in);
@@ -92,7 +112,7 @@ public class Exam0210 {
         continue;
       }
 
-      // 스레드풀에서 스레드를 받았다면 카운트를 시작시킨다.
+      // 스레드의 카운트를 설정한다. 그러면 카운트를 시작할 것이다.
       t.setCount(count);
     }
 

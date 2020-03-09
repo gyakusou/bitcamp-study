4e755ff844133db7b27816d5034a76b22679f3b4
hongseongjun90 <hongseongjun90@gmail.com>
2020-03-09 17:34:21 +0900

    foreach 3 bind

--- bitcamp-java/src/main/java/com/eomcs/mybatis/ex03/Exam0280.java
+++ bitcamp-java/src/main/java/com/eomcs/mybatis/ex03/Exam0280.java
@@ -1,8 +1,7 @@
-// dynamic sql 다루기 - <foreach> 사용법 III
+// // dynamic sql 다루기 - <bind> 사용법
 package com.eomcs.mybatis.ex03;
 
 import java.io.InputStream;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
@@ -11,7 +10,7 @@ import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
-public class Exam0270 {
+public class Exam0280 {
 
   public static void main(String[] args) throws Exception {
     InputStream inputStream = Resources.getResourceAsStream(//
@@ -29,18 +28,15 @@ public class Exam0270 {
 
     Scanner keyScan = new Scanner(System.in);
 
-    System.out.print("검색? ");
-    String[] values = keyScan.nextLine().split(" ");
-
-    ArrayList<Object> words = new ArrayList<>();
-    for (String value : values) {
-      words.add(value.trim());
-    }
-    params.put("words", words);
+    System.out.print("제목? ");
+    String value = keyScan.nextLine();
+    params.put("title", value);
 
     keyScan.close();
 
-    List<Board> list = sqlSession.selectList("BoardMapper.select25", params);
+    List<Board> list = sqlSession.selectList("BoardMapper.select26", params);
+    // <bind> 태그를 사용하면
+    // => like 문의 문자열 패턴을 만들 때 편하다.
 
     for (Board board : list) {
       System.out.printf("%d, %s, %s, %s, %d\n", //
--- bitcamp-java/src/main/java/com/eomcs/mybatis/ex03/Exam0310.java
+++ bitcamp-java/src/main/java/com/eomcs/mybatis/ex03/Exam0310.java
@@ -1,8 +1,7 @@
-// dynamic sql 다루기 - <foreach> 사용법 III
+// <sql> 사용법
 package com.eomcs.mybatis.ex03;
 
 import java.io.InputStream;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
@@ -11,7 +10,7 @@ import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
-public class Exam0270 {
+public class Exam0310 {
 
   public static void main(String[] args) throws Exception {
     InputStream inputStream = Resources.getResourceAsStream(//
@@ -29,18 +28,18 @@ public class Exam0270 {
 
     Scanner keyScan = new Scanner(System.in);
 
-    System.out.print("검색? ");
-    String[] values = keyScan.nextLine().split(" ");
-
-    ArrayList<Object> words = new ArrayList<>();
-    for (String value : values) {
-      words.add(value.trim());
-    }
-    params.put("words", words);
+    System.out.print("제목? ");
+    String value = keyScan.nextLine();
+    params.put("title", value);
 
     keyScan.close();
 
-    List<Board> list = sqlSession.selectList("BoardMapper.select25", params);
+    List<Board> list = sqlSession.selectList("BoardMapper2.select26", params);
+    // <sql> 태그를 사용하면
+    // => 여러 SQL 문에서 중복적으로 사용하는 SQL 일부를 별도로 관리할 수 있다.
+    // => 중복된 SQL을 메서드처럼 별도로 관리하기 때문에
+    // 사용하여 SQL을 관리하기가 쉬워진다.
+
 
     for (Board board : list) {
       System.out.printf("%d, %s, %s, %s, %d\n", //

b57a42e3bf2e0bc3c8a2650bfa762f61707f9274
hongseongjun90 <hongseongjun90@gmail.com>
2020-03-09 12:49:04 +0900

    where 개선

---
+++ bitcamp-java/src/main/java/com/eomcs/mybatis/ex03/Exam1160.java
@@ -0,0 +1,72 @@
+// // dynamic sql 다루기 - <where> 사용 후
+package com.eomcs.mybatis.ex03;
+
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class Exam1160 {
+
+  public static void main(String[] args) throws Exception {
+    InputStream inputStream = Resources.getResourceAsStream(//
+        "com/eomcs/mybatis/ex03/mybatis-config.xml");
+    SqlSessionFactory factory = //
+        new SqlSessionFactoryBuilder().build(inputStream);
+
+    SqlSession sqlSession = factory.openSession();
+
+    // 실행 예:
+    // => 복합 검색하기, 여러개의 조건을 합쳐서 검색하기
+    // 제목+내용 검색하는 방법
+
+    // SQL Mapper 에 여러개의 파라미터 값을 넘길 때 주로 Map을 사용한다.s
+    HashMap<String, Object> params = new HashMap<>();
+
+    Scanner keyScan = new Scanner(System.in);
+
+    System.out.print("번호? ");
+    String value = keyScan.nextLine();
+    if (value.length() > 0) {
+      params.put("no", value);
+    }
+
+    System.out.print("제목? ");
+    value = keyScan.nextLine();
+    if (value.length() > 0) {
+      params.put("title", value);
+    }
+
+    System.out.print("내용? ");
+    value = keyScan.nextLine();
+    if (value.length() > 0) {
+      params.put("content", value);
+    }
+
+    keyScan.close();
+
+    List<Board> list = sqlSession.selectList("BoardMapper.select7", params);
+
+    // select7의 이점
+    // => or/and 앞에 조건이 없을 때 or/and를 자동으로 제거한다.
+    //
+
+    for (Board board : list) {
+      System.out.printf("%d, %s, %s, %s, %d\n", //
+          board.getNo(), //
+          board.getTitle(), //
+          board.getContent(), //
+          board.getRegisteredDate(), //
+          board.getViewCount());
+    }
+
+    sqlSession.close();
+  }
+
+}
+
+

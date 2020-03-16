5a5e9b8559077f8aeaee660c8ec8be6f7f7d1e53
hongseongjun90 <hongseongjun90@gmail.com>
2020-03-16 11:13:35 +0900

    AppCon03

---
+++ bitcamp-porject-server/src/main/java/com/eomcs/util/ApplicationContext03.java
@@ -0,0 +1,58 @@
+package com.eomcs.util;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.util.ArrayList;
+import org.apache.ibatis.io.Resources;
+
+// 역할:
+// - 클래스를 찾아 객체를 생성한다.
+// - 객체가 일을 하는데 필요로 하는 의존 객체를 주입한다.
+// - 객체를 생성과 소멸을 관리한다.
+//
+public class ApplicationContext03 {
+
+  // 클래스 이름을 담을 저장소
+  ArrayList<String> classNames = new ArrayList<>();
+
+  public ApplicationContext03(String packageName) throws Exception {
+    // 패키지의 실제 파일 시스템 경로를 알아낸다.
+    // System.out.println("ApplacationContext: " + packageName);
+
+    String packagePath = packageName.replace('.', '/');
+    // System.out.println("ApplacationContext: " + packagePath);
+
+    File path = Resources.getResourceAsFile(//
+        packagePath /* 패키지 명을 파일 시스템 경로로 바꿔서 전달한다. */);
+    // System.out.println("ApplicationContext: " + path.getAbsolutePath());
+
+    // 해당 경로를 뒤져서 모든 클래스의 이름을 알아낸다.
+    findClasses(path, packageName);
+
+  }
+
+  private void findClasses(File path, String packageName) {
+    File[] files = path.listFiles(new FileFilter() {
+      @Override
+      public boolean accept(File file) {
+        if (file.isDirectory() // 디렉토리면 트루
+            || (file.getName().endsWith(".class") // 조건을 만족시키면 true (.class 이면서 파일에 $를 포함 하면 안된다.)
+                && !file.getName().contains("$"))) //
+          return true;
+        return false;
+      }
+    });
+    for (File f : files) {
+      String classOrPackageName = //
+          packageName + "." + f.getName().replace(".class", "");
+      if (f.isFile()) {
+        // System.out.println("ApplicationContext: " + classOrPackageName);
+        classNames.add(classOrPackageName);
+      } else {
+        findClasses(f, classOrPackageName);
+      }
+    }
+  }
+}
+
+

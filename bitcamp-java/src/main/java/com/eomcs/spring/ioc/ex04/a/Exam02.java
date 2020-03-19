16c252229ec0769a142d6a904dbed816a8a0937f
hongseongjun90 <hongseongjun90@gmail.com>
2020-03-19 14:55:25 +0900

    temp

--- bitcamp-java/src/main/java/com/eomcs/spring/ioc/ex04/a/Exam02.java
+++ bitcamp-java/src/main/java/com/eomcs/spring/ioc/ex04/a/Exam02.java
@@ -1,26 +1,24 @@
-// 프로퍼티 호출 
-package com.eomcs.spring.ioc.ex04;
+// 프로퍼티 값 지정하기
+package com.eomcs.spring.ioc.ex04.a;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.eomcs.spring.ioc.SpringUtils;
-
-public class Exam01 {
+public class Exam02 {
 
   public static void main(String[] args) {
-    ApplicationContext iocContainer = new ClassPathXmlApplicationContext(
-        "bitcamp/java106/step04/application-context-01.xml");
+    ApplicationContext iocContainer = new ClassPathXmlApplicationContext( //
+        "com/eomcs/spring/ioc/ex04/a/application-context.xml");
+
+    // 객체의 프로퍼티 값이 제대로 설정되었는지 확인한다.
 
     System.out.println(iocContainer.getBean("c1"));
     System.out.println(iocContainer.getBean("c2"));
     System.out.println(iocContainer.getBean("c3"));
     System.out.println(iocContainer.getBean("c4"));
+
   }
 
 }
-
-
-
 
 

e9638eca98428ffed187a569f9ec3e86d6a41846
hongseongjun90 <hongseongjun90@gmail.com>
2020-03-19 10:22:35 +0900

    temp

--- bitcamp-java/src/main/java/com/eomcs/spring/ioc/ex01/b/Exam01.java
+++ bitcamp-java/src/main/java/com/eomcs/spring/ioc/ex01/b/Exam01.java
@@ -2,24 +2,22 @@
 package com.eomcs.spring.ioc.ex01.b;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class Exam01 {
   public static void main(String[] args) {
 
     // 2) 운영체제의 파일 시스템에서 설정 파일을 찾는 IoC 컨테이너
-    // => 설정 파일 경로를 지정할 때 직접 파일 경로를 지정해야 한다.
-    // => 주의!
-    // 설정 파일 경로를 지정할 때 URL 형식으로 지정해야 한다.
-    // URL 형식에서는 파일 시스템을 가리킬 때 다음의 접두어를 붙인다.
-    // file://
-    ApplicationContext iocContainer2 = new FileSystemXmlApplicationContext(
-        "file:///Users/eomjinyoung/git/bitcamp-java-2018-12/java-basic/bin/main/ch29/a/application-context.xml");
-
-    // 3) 자바 클래스 파일의 애노테이션으로부터 설정 정보를 추출한다. = java config 라 부른다.
-    // => 생성자에 설정 정보를 갖고 있는 클래스의 타입 정보를 넘긴다.
-    ApplicationContext iocContainer3 = new AnnotationConfigApplicationContext(AppConfig.class);
+    // => 자바 classpath가 아닌 다른 폴더에 설정파일이 있을 경우 사용한다.
+    // => 설정 파일 경로를 지정할 때 파일 시스템 경로를 지정해야 한다.
+    // => 단, URL 형식으로 지정해야 한다.
+    // => 예)
+    // file://설정파일경로
+    // URL 형식에서는 파일 시스템을 가리킬 때 'file://' 를 붙인다.
+    // 역슬래시는 \\ 두개 슬래시는 / 한개 붙이면 된다.
+    //
+    ApplicationContext iocContainer = new FileSystemXmlApplicationContext(
+        "file:///Users\\user\\git\\bitcamp-study\\bitcamp-java\\src\\main\\java\\com\\eomcs\\spring\\ioc\\ex01\\b\\application-context.xml");
 
     System.out.println("실행 완료!");
   }

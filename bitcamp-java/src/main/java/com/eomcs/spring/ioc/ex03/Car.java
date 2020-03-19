268d32b53da254f2e1361ea8776d865468d65d09
hongseongjun90 <hongseongjun90@gmail.com>
2020-03-19 14:21:59 +0900

    temp

--- bitcamp-java/src/main/java/com/eomcs/spring/ioc/ex03/Car.java
+++ bitcamp-java/src/main/java/com/eomcs/spring/ioc/ex03/Car.java
@@ -32,6 +32,8 @@ public class Car {
   }
 
 
+
+
   @Override
   public String toString() {
     return "Car [model=" + model + ", maker=" + maker + ", cc=" + cc + "]";
@@ -40,20 +42,25 @@ public class Car {
   public String getModel() {
     return model;
   }
+
   public void setModel(String model) {
     System.out.println("setModel() 호출됨!");
     this.model = model;
   }
+
   public String getMaker() {
     return maker;
   }
+
   public void setMaker(String maker) {
     System.out.println("setMaker() 호출됨!");
     this.maker = maker;
   }
+
   public int getCc() {
     return cc;
   }
+
   public void setCc(int cc) {
     System.out.println("setCc() 호출됨!");
     this.cc = cc;

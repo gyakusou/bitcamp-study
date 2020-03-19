0e442c85e8c0599ecccfdfd0647681c0d625b4f3
hongseongjun90 <hongseongjun90@gmail.com>
2020-03-19 14:24:49 +0900

    temp

--- bitcamp-java/src/main/java/com/eomcs/spring/ioc/ex03/c/Exam01.java
+++ bitcamp-java/src/main/java/com/eomcs/spring/ioc/ex03/c/Exam01.java
@@ -1,4 +1,4 @@
-// 생성자의 파라미터 값을 지정하는 방법2
+// 생성자의 파라미터 값을 형변환 할 수 없는 경우 - 예외발생
 package com.eomcs.spring.ioc.ex03.c;
 
 import org.springframework.context.ApplicationContext;
@@ -8,7 +8,7 @@ public class Exam01 {
 
   public static void main(String[] args) {
     ApplicationContext iocContainer =
-        new ClassPathXmlApplicationContext("com/eomcs/spring/ioc/ex03/b/application-context.xml");
+        new ClassPathXmlApplicationContext("com/eomcs/spring/ioc/ex03/c/application-context.xml");
 
     // SpringUtils.printBeanNames(iocContainer);
   }

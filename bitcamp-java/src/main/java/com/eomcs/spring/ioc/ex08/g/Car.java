4b12d8a571b2e11489c2359429ba8a1105ef7c2a
hongseongjun90 <hongseongjun90@gmail.com>
2020-03-20 16:35:29 +0900

    temp

--- bitcamp-java/src/main/java/com/eomcs/spring/ioc/ex08/g/Car.java
+++ bitcamp-java/src/main/java/com/eomcs/spring/ioc/ex08/g/Car.java
@@ -1,68 +1,77 @@
-package com.eomcs.spring.ioc.ex08;
+package com.eomcs.spring.ioc.ex08.g;
 
 import java.sql.Date;
-
 import org.springframework.beans.factory.annotation.Autowired;
 
-// 의존 객체 Engine 주입 - 인스턴스 변수에 @Autowired를 붙여도 된다.
-public class Car4 {
-  String model;
-  String maker;
-  int cc;
-  boolean auto;
-  Date createdDate;
+// 의존 객체 Engine 주입 - 필수 또는 선택
+public class Car {
+  private String model;
+  private String maker;
+  private int cc;
+  private boolean auto;
+  private Date createdDate;
+  @Autowired
+  private Engine engine;
 
-  Engine engine;
 
-  // 만약 실행 중에 의존 객체를 교체해서는 안된다면,
-  // 임의로 의존 객체를 교체하지 못하도록 final을 붙여라! 
-  public Car4(final Engine engine) {
-    System.out.println("Car4()");
-    this.engine = engine;
+  public Car() {
+    System.out.println("Car()");
   }
 
 
   @Override
   public String toString() {
-    return "Car [model=" + model + ", maker=" + maker + ", cc=" + cc + ", auto=" + auto + ", createdDate="
-        + createdDate + ", engine=" + engine + "]";
+    return "Car [model=" + model + ", maker=" + maker + ", cc=" + cc + ", auto=" + auto
+        + ", createdDate=" + createdDate + ", engine=" + engine + "]";
   }
+
   public Engine getEngine() {
     return engine;
   }
 
-  public void setEngine(Engine engine) {
-    System.out.println("Car.setEngine()");
-    this.engine = engine;
-  }
+
+  //
+  // public void setEngine(Engine engine) {
+  // System.out.println("Car.setEngine()");
+  // this.engine = engine;
+  // }
 
   public boolean isAuto() {
     return auto;
   }
+
   public void setAuto(boolean auto) {
     this.auto = auto;
   }
+
   public Date getCreatedDate() {
     return createdDate;
   }
+
   public void setCreatedDate(Date createdDate) {
     this.createdDate = createdDate;
   }
+
   public String getModel() {
     return model;
   }
+
   public void setModel(String model) {
     this.model = model;
   }
+
   public String getMaker() {
     return maker;
   }
+
   public void setMaker(String maker) {
     this.maker = maker;
   }
+
   public int getCc() {
     return cc;
   }
+
   public void setCc(int cc) {
     this.cc = cc;
   }

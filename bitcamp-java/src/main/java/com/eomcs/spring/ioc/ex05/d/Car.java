0fe6018681320dcaeec11aa05b438255a9fb995d
hongseongjun90 <hongseongjun90@gmail.com>
2020-03-20 10:16:12 +0900

    temp

--- bitcamp-java/src/main/java/com/eomcs/spring/ioc/ex05/d/Car.java
+++ bitcamp-java/src/main/java/com/eomcs/spring/ioc/ex05/d/Car.java
@@ -1,8 +1,8 @@
-package com.eomcs.spring.ioc.ex05;
+package com.eomcs.spring.ioc.ex05.d;
 
 import java.util.Properties;
 
-public class Car4 {
+public class Car {
   String model;
   String maker;
   int cc;
@@ -10,29 +10,37 @@ public class Car4 {
 
   @Override
   public String toString() {
-    return "Car4 [model=" + model + ", maker=" + maker + ", cc=" + cc + ", options=" + options + "]";
+    return "Car [model=" + model + ", maker=" + maker + ", cc=" + cc + ", options=" + options + "]";
   }
+
   public Properties getOptions() {
     return options;
   }
+
   public void setOptions(Properties options) {
     this.options = options;
   }
+
   public String getModel() {
     return model;
   }
+
   public void setModel(String model) {
     this.model = model;
   }
+
   public String getMaker() {
     return maker;
   }
+
   public void setMaker(String maker) {
     this.maker = maker;
   }
+
   public int getCc() {
     return cc;
   }
+
   public void setCc(int cc) {
     this.cc = cc;
   }
--- bitcamp-java/src/main/java/com/eomcs/spring/ioc/ex05/d/Engine.java
+++ bitcamp-java/src/main/java/com/eomcs/spring/ioc/ex05/d/Engine.java
@@ -1,4 +1,4 @@
-package com.eomcs.spring.ioc.ex05;
+package com.eomcs.spring.ioc.ex05.d;
 
 public class Engine {
   String maker;

1701882a5c91d568527b35e8597dc6b7c42d089f
hongseongjun90 <hongseongjun90@gmail.com>
2020-03-23 10:30:38 +0900

    temp

--- bitcamp-java/src/main/java/com/eomcs/spring/ioc/ex09/b/Exam01.java
+++ bitcamp-java/src/main/java/com/eomcs/spring/ioc/ex09/b/Exam01.java
@@ -1,25 +1,21 @@
 // 객체 자동 생성 - @Component 애노테이션
-package com.eomcs.spring.ioc.ex09;
+package com.eomcs.spring.ioc.ex09.b;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 import com.eomcs.spring.ioc.SpringUtils;
 
 public class Exam01 {
 
   public static void main(String[] args) {
-    ApplicationContext iocContainer = new ClassPathXmlApplicationContext(
-        "bitcamp/java106/step09/application-context-01.xml");
+    // <context:annotation-config> 태그 생략하기
+    ApplicationContext iocContainer = new ClassPathXmlApplicationContext( //
+        "com/eomcs/spring/ioc/ex09/b/application-context.xml");
 
-    SpringUtils.printBeanNames(iocContainer);
+    SpringUtils.printBeanList(iocContainer);
 
-    System.out.println(iocContainer.getBean("car"));
   }
 
 }
-
-
-
 
 

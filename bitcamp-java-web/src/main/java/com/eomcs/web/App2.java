968b1afe47e78e6a0af829f2a18c356f94cb290d
hongseongjun90 <hongseongjun90@gmail.com>
2020-04-03 12:38:54 +0900

    temp

--- bitcamp-java-web/src/main/java/com/eomcs/web/App2.java
+++ bitcamp-java-web/src/main/java/com/eomcs/web/App2.java
@@ -3,12 +3,13 @@
  */
 package bitcamp;
 
-public class App {
+public class App2 {
     public String getGreeting() {
         return "Hello world.";
     }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        System.out.println(new App2().getGreeting());
     }
+    
 }

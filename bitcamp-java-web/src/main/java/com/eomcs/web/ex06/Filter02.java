17571db4a6f213a285bef034b14fe2f8ef67b520
hongseongjun90 <hongseongjun90@gmail.com>
2020-04-07 14:21:44 +0900

    temp

--- bitcamp-java-web/src/main/java/com/eomcs/web/ex06/Filter02.java
+++ bitcamp-java-web/src/main/java/com/eomcs/web/ex06/Filter02.java
@@ -1,5 +1,5 @@
-// 필터 초기화 파라미터 : web.xml에서 설정한 값 가져오기
-package bitcamp.ex06;
+// 필터 초기화 파라미터 : 애노테이션으로 설정한 값 가져오기
+package com.eomcs.web.ex06;
 
 import java.io.IOException;
 import javax.servlet.Filter;
@@ -9,28 +9,29 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 
-public class Filter01 implements Filter {
+// @WebFilter(
+// value="/ex06/*",
+// initParams={
+// @WebInitParam(name="encoding", value="EUC-KR")
+// })
+public class Filter02 implements Filter {
 
   FilterConfig filterConfig;
-  
+
   @Override
   public void init(FilterConfig filterConfig) throws ServletException {
     this.filterConfig = filterConfig;
   }
-  
+
   @Override
   public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
       throws IOException, ServletException {
-    
+
     // 필터의 DD 설정으로 지정한 파라미터 값 가져오기
-    System.out.printf("ex06.Filter01 : encoding=%s\n", 
-        filterConfig.getInitParameter("encoding"));
-    
+    System.out.printf("ex06.Filter02 : encoding=%s\n", filterConfig.getInitParameter("encoding"));
+
     chain.doFilter(request, response);
   }
 }
-
-
-
 
 
